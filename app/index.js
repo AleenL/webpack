77b039d1c306ca781859717eb4f0030239d370d9
784757555@qq.com <784757555@qq.com>
2017-04-23 12:36:57 +0800

    last

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import $ from 'jquery';
+import foo from './foo';
 
 
 function component () {
@@ -12,3 +13,4 @@ function component () {
 }
 
 document.body.appendChild(component());
+console.log(foo())
